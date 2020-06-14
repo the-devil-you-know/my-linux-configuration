c7d349aba42aee8a58a2300853c2c012048c3cce
Ivan Medvedev <medvedev.is@outlook.com>
2020-06-14 12:04:16 +0700

    clean set_ru_layout.sh

--- set_ru_layout.sh
+++ set_ru_layout.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# So tricky, but it fixes problems with Intellij Idea
-setxkbmap -layout en
-xdotool key Caps_Lock
-xdotool key Caps_Lock
-
 xkbcomp "`dirname $0`/xkb_keymap_ru" $DISPLAY
 killall xcape
 xcape -e 'Mode_switch=Escape'
